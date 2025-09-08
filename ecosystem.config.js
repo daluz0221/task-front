1f0ea39851e3dc0f63a90124151132c9b5a46a70
Luis Felipe <lfelipe.desarrollador@gmail.com>
2025-09-07 21:39:53 -0500

    corrigiendo ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,11 @@ module.exports = {
     {
       name: "frontend",                        // Nombre del proceso en PM2
       script: "node_modules/next/dist/bin/next",
-      args: ["start", "-p", "3000"],                   // Puerto donde corre Next.js
+      args: "start",                   // Puerto donde corre Next.js
       cwd: "/var/www/frontend",                // Ruta de tu proyecto
       env: {
-        NODE_ENV: "production",                // Entorno
+        NODE_ENV: "production",
+        PORT: 3000                
       },
     },
   ],
