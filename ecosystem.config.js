ecb0701f8078e2825a7e79af8c79bf557060fbac
Luis Felipe <lfelipe.desarrollador@gmail.com>
2025-09-07 20:38:32 -0500

    agregando ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "frontend",                        // Nombre del proceso en PM2
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",                   // Puerto donde corre Next.js
+      cwd: "/var/www/frontend",                // Ruta de tu proyecto
+      env: {
+        NODE_ENV: "production",                // Entorno
+      },
+    },
+  ],
+};
